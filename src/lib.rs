98324c5dc61a5e604323254e4b41fa83bd7d295a
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-17 08:47:48 -0500

    #4 verify with bcrypt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,33 @@
-use std::{ffi::{c_char, CString, CStr}, iter};
+use std::{ffi::{c_char, CString, CStr}};
 
 use bcrypt::{hash, verify, DEFAULT_COST};
 
 #[no_mangle]
-pub extern "C" fn bcrypt_hash(pass_to_hash: &str) -> *mut c_char {
-    let hashed_password = CString::new(hash(pass_to_hash, DEFAULT_COST).unwrap()).unwrap();
+pub extern "C" fn bcrypt_hash(pass_to_hash: *const c_char) -> *mut c_char {
+    let string_pass = unsafe {
+        assert!(!pass_to_hash.is_null());
+
+        CStr::from_ptr(pass_to_hash)
+    }.to_str().unwrap();
+
+    let hashed_password = CString::new(hash(string_pass, DEFAULT_COST).unwrap()).unwrap();
     return hashed_password.into_raw();
+}
+
+#[no_mangle]
+pub extern "C" fn bcrypt_verify(pass: *const c_char, hash: *const c_char) -> bool {
+    let string_pass = unsafe {
+        assert!(!pass.is_null());
+
+        CStr::from_ptr(pass)
+    }.to_str().unwrap();
+
+    let string_hash = unsafe {
+        assert!(!hash.is_null());
+
+        CStr::from_ptr(hash)
+    }.to_str().unwrap();
+    return verify(string_pass, string_hash).unwrap();
 }
 
 // must pass the string back into Rust to deallocate the memory as it is NOT thread safe

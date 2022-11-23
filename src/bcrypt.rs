f7ffe90ec24133957ea7d42e8592e87a55c7887b
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-22 21:47:41 -0500

    removing deallocation

--- src/bcrypt.rs
+++ src/bcrypt.rs
@@ -29,14 +29,3 @@ pub extern "C" fn bcrypt_verify(pass: *const c_char, hash: *const c_char) -> boo
     }.to_str().unwrap();
     return verify(string_pass, string_hash).unwrap();
 }
-
-// must pass the string back into Rust to deallocate the memory as it is NOT thread safe
-#[no_mangle]
-pub extern "C" fn bcrypt_hash_free(s: *mut c_char) {
-    unsafe {
-        if s.is_null() {
-            return;
-        }
-        CString::from_raw(s)
-    };
-}

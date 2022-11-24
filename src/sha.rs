dfdf32a81ed0cbd03785e8b22425497c80ec28f3
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-24 01:11:52 -0500

    #5 verify sha512

--- src/sha.rs
+++ src/sha.rs
@@ -1,5 +1,6 @@
 use std::ffi::{c_char, CStr, CString};
 
+use rsa::signature::digest::core_api::CtVariableCoreWrapper;
 use sha_crypt::{Sha512Params, sha512_simple, sha512_check};
 
 #[no_mangle]
@@ -14,3 +15,20 @@ pub extern "C" fn sha512_hash_password(password: *const c_char) -> *mut c_char {
     let hashed_password: CString = CString::new(sha512_simple(string_password, &params).unwrap()).unwrap();
     return hashed_password.into_raw();
 } 
+
+#[no_mangle]
+pub extern "C" fn sha512_verify_password(password: *const c_char, hashed_password: *const c_char) -> bool {
+    let string_password = unsafe {
+        assert!(!password.is_null());
+
+        CStr::from_ptr(password)
+    }.to_str().unwrap();
+
+    let string_hashed_password = unsafe {
+        assert!(!hashed_password.is_null());
+
+        CStr::from_ptr(hashed_password)
+    }.to_str().unwrap();
+
+    return sha512_check(string_password, &string_hashed_password).is_ok()
+}

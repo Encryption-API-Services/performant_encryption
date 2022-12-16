f07d56bde87af609e9931ec2357ec83b4fdfbcae
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-15 20:43:10 -0500

    #14 scrypt verify test

--- src/scrypt.rs
+++ src/scrypt.rs
@@ -51,3 +51,17 @@ pub extern "C" fn scrypt_verify(pass_to_check: *const c_char, hash_to_check: *co
     let parsed_hash = PasswordHash::new(&string_hash).unwrap();
     return Scrypt.verify_password(string_pass.as_bytes(), &parsed_hash).is_ok();
 }
+
+#[test]
+fn scrypt_verify_test() {
+    let password = "NotThePasswordYouAreLookingFor";
+    let password_cstr = CString::new(password).unwrap();
+    let password_bytes = password_cstr.as_bytes_with_nul();
+    let password_ptr = password_bytes.as_ptr() as *const i8;
+    let hash = scrypt_hash(password_ptr);
+    let hash_ctr = unsafe {CString::from_raw(hash)};
+    let hashed_bytes = hash_ctr.as_bytes_with_nul();
+    let hashed_ptr = hashed_bytes.as_ptr() as *const i8;
+    let is_valid = scrypt_verify(password_ptr, hashed_ptr);
+    assert_eq!(true, is_valid);
+}

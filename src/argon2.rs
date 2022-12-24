87c96d6f8f193d6116ed0d275d947c82bf4d3d52
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-12-23 23:06:32 -0500

    #23 adding argon2

--- src/argon2.rs
+++ src/argon2.rs
@@ -8,6 +8,38 @@ use argon2::{
     Argon2
 };
 
+#[no_mangle]
+pub extern "C" fn argon2_verify(hashed_pass: *const c_char, password: *const c_char) -> bool {
+    let hashed_pass_string = unsafe {
+        assert!(!hashed_pass.is_null());
+        CStr::from_ptr(hashed_pass)
+    }.to_str().unwrap();
+
+    let password_string = unsafe {
+        assert!(!password.is_null());
+        CStr::from_ptr(password)
+    }.to_str().unwrap().as_bytes();
+
+    let parsed_hash = PasswordHash::new(&hashed_pass_string).unwrap();
+    let result = Argon2::default().verify_password(password_string, &parsed_hash).is_ok();
+    return result
+}
+
+#[test]
+fn argon2_verify_test() {
+    let password = "PasswordToVerify";
+    let password_cstr = CString::new(password).unwrap();
+    let password_bytes = password_cstr.as_bytes_with_nul();
+    let password_ptr = password_bytes.as_ptr() as *const i8;
+    let hashed_password = argon2_hash(password_ptr);
+    let hashed_password_ctr = unsafe {CString::from_raw(hashed_password)};
+    let hashed_password_bytes = hashed_password_ctr.as_bytes_with_nul();
+    let hashed_password_ptr = hashed_password_bytes.as_ptr() as *const i8;
+    let is_valid = argon2_verify(hashed_password_ptr, password_ptr);
+    assert_eq!(true, is_valid);
+
+}
+
 
 #[no_mangle]
 pub extern "C" fn argon2_hash(pass_to_hash: *const c_char) -> *mut c_char {

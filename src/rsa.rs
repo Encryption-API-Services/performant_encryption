db37fc21b91fc6b8fa2027a2dc42ace958c0bc00
Mike Mulchrone <mtmulch0191@outlook.com>
2022-12-26 13:12:28 -0500

    #26 rsa encrypt decrypt (#28)
    * encrypting a string value with rsa key
    
    * #26 return encrypted string pointer
    
    * #26 rsa encrypt and decrypt
    
    Co-authored-by: Mike Mulchrone <mulchronemike0191@gmail.com>

--- src/rsa.rs
+++ src/rsa.rs
@@ -1,13 +1,54 @@
-use std::{ffi::{CString, c_char}};
+use std::{ffi::{CString, c_char, CStr}};
 
 use rand::rngs::OsRng;
-use rsa::{RsaPrivateKey, RsaPublicKey, pkcs8::EncodePrivateKey, pkcs1::EncodeRsaPublicKey};
+use rsa::{RsaPrivateKey, RsaPublicKey, pkcs8::{EncodePrivateKey, DecodePrivateKey}, pkcs1::{EncodeRsaPublicKey, DecodeRsaPublicKey}, PublicKey, PaddingScheme};
 
 #[repr(C)]
 pub struct RsaKeyPair {
     pub pub_key: *mut c_char,
     pub priv_key: *mut c_char
 }
+
+#[no_mangle]
+pub extern "C" fn rsa_encrypt(pub_key: *const c_char, data_to_encrypt: *const c_char) -> *mut c_char {
+    let pub_key_string = unsafe {
+        assert!(!pub_key.is_null());
+
+        CStr::from_ptr(pub_key)
+    }.to_str().unwrap();
+
+    let data_to_encrypt_bytes = unsafe {
+        assert!(!data_to_encrypt.is_null());
+
+        CStr::from_ptr(data_to_encrypt)
+    }.to_str().unwrap().as_bytes();
+
+    let public_key = RsaPublicKey::from_pkcs1_pem(pub_key_string).unwrap();
+    let mut rng = rand::thread_rng();
+    let encrypted_bytes = public_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &data_to_encrypt_bytes).unwrap();
+    return CString::new(base64::encode(encrypted_bytes)).unwrap().into_raw();
+}
+
+#[no_mangle]
+pub extern "C" fn rsa_decrypt(priv_key: *const c_char, data_to_decrypt: *const c_char) -> *mut c_char {
+    let priv_key_string = unsafe {
+        assert!(!priv_key.is_null());
+
+        CStr::from_ptr(priv_key)
+    }.to_str().unwrap();
+
+    let data_to_decrypt_string = unsafe {
+        assert!(!data_to_decrypt.is_null());
+        CStr::from_ptr(data_to_decrypt)
+    }.to_str().unwrap();
+
+    let data_to_decrypt_bytes = base64::decode(data_to_decrypt_string).unwrap();
+
+    let private_key = RsaPrivateKey::from_pkcs8_pem(priv_key_string).unwrap();
+    let decrypted_bytes = private_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &data_to_decrypt_bytes).expect("failed to decrypt");
+    return CString::new(decrypted_bytes).unwrap().into_raw()
+}
+
 
 #[no_mangle]
 pub extern "C" fn get_key_pair(key_size: usize) -> RsaKeyPair {

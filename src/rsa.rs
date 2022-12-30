8d55f8cba3e18753752cee0b7bcb3b298b989337
Mike Mulchrone <mtmulch0191@outlook.com>
2022-12-30 01:00:23 -0500

    #27 rsa sign verify (#30)
    * #27 rsa sign and verify nonffi
    
    * non ffi tests
    
    * non ffi
    
    * rsa sign
    
    * struct for C
    
    * #27 rsa verify

--- src/rsa.rs
+++ src/rsa.rs
@@ -1,12 +1,19 @@
 use std::{ffi::{CString, c_char, CStr}};
 
 use rand::rngs::OsRng;
-use rsa::{RsaPrivateKey, RsaPublicKey, pkcs8::{EncodePrivateKey, DecodePrivateKey}, pkcs1::{EncodeRsaPublicKey, DecodeRsaPublicKey}, PublicKey, PaddingScheme};
+use rsa::RsaPrivateKey;
+use rsa::{RsaPublicKey, pkcs8::{EncodePrivateKey, DecodePrivateKey}, pkcs1::{EncodeRsaPublicKey, DecodeRsaPublicKey}, PublicKey, PaddingScheme};
 
 #[repr(C)]
 pub struct RsaKeyPair {
     pub pub_key: *mut c_char,
     pub priv_key: *mut c_char
+}
+
+#[repr(C)]
+pub struct RsaSignResult {
+    pub signature: *mut c_char,
+    pub public_key: *mut c_char
 }
 
 #[no_mangle]
@@ -75,6 +82,67 @@ pub extern "C" fn get_key_pair(key_size: usize) -> RsaKeyPair {
         priv_key: CString::new(private_key.to_pkcs8_pem(rsa::pkcs8::LineEnding::LF).unwrap().to_string()).unwrap().into_raw()
     };
     return key_pair;
+}
+
+#[no_mangle]
+pub extern "C" fn rsa_sign(data_to_sign: *mut c_char, key_size: usize) -> RsaSignResult {
+    let data_to_sign = unsafe {
+        assert!(!data_to_sign.is_null());
+
+        CStr::from_ptr(data_to_sign)
+    }.to_bytes();
+    let mut rng: OsRng = OsRng;
+    let private_key = RsaPrivateKey::new(&mut rng, key_size).expect("failed to generate a key");
+    let signature = CString::new(base64::encode(private_key.sign(PaddingScheme::new_pkcs1v15_sign_raw(), data_to_sign).unwrap())).unwrap().into_raw();
+    let public_key = private_key.to_public_key();
+    let public_key = CString::new(public_key.to_pkcs1_pem(rsa::pkcs1::LineEnding::LF).unwrap().to_string()).unwrap().into_raw();
+    return RsaSignResult { signature: signature, public_key: public_key }
+}
+
+#[test]
+fn rsa_sign_nonffi_test() {
+    let mut rng: OsRng = OsRng;
+    let private_key: RsaPrivateKey = RsaPrivateKey::new(&mut rng, 2094).expect("failed to generate a key");
+    let public_key: RsaPublicKey = private_key.to_public_key();
+    let data = b"testing";
+    let signature = private_key.sign(PaddingScheme::new_pkcs1v15_sign_raw(), data).unwrap();
+    assert_ne!(data.as_slice(), signature);
+}
+
+#[no_mangle]
+pub extern "C" fn rsa_verify(public_key: *mut c_char, data_to_verify: *mut c_char, signature: *mut c_char) -> bool {
+    let public_key_string = unsafe {
+        assert!(!public_key.is_null());
+        CStr::from_ptr(public_key)
+    }.to_str().unwrap();
+    let data_to_verify_bytes = unsafe {
+        assert!(!data_to_verify.is_null());
+        CStr::from_ptr(data_to_verify)
+    }.to_bytes();
+    let signature_string = unsafe {
+        assert!(!signature.is_null());
+        CStr::from_ptr(signature)
+    }.to_str().unwrap();
+    let signature_bytes = base64::decode(signature_string).unwrap();
+
+    let public_key = RsaPublicKey::from_pkcs1_pem(public_key_string).unwrap();
+    let verified = public_key.verify(PaddingScheme::new_pkcs1v15_sign_raw(), &data_to_verify_bytes, &signature_bytes);
+    if verified.is_err() == false {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+#[test]
+fn rsa_verify_nonffi_test() {
+    let mut rng: OsRng = OsRng;
+    let private_key: RsaPrivateKey = RsaPrivateKey::new(&mut rng, 2094).expect("failed to generate a key");
+    let public_key: RsaPublicKey = private_key.to_public_key();
+    let data =  "testing".as_bytes();
+    let signature = private_key.sign(PaddingScheme::new_pkcs1v15_sign_raw(), data).unwrap();
+    let verified = public_key.verify(PaddingScheme::new_pkcs1v15_sign_raw(), &data, &signature);
+    assert_eq!(verified.is_err(), false);
 }
 
 #[test]
